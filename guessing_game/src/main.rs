cc8db63ebeec2a8bfd1ece16b5874feb92667e4a
birkin <birkin.diana@gmail.com>
2020-04-02 07:19:08 -0400

    implements guessing game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,30 +8,42 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("secret_number, ```{:?}```", secret_number);
+    // println!("secret_number, ```{:?}```", secret_number);
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
 
-    let guess: u32 = guess.trim().parse()
-        .expect( "Please type a number!" );
+        // let guess: u32 = guess.trim()
+        //     .parse()
+        //     .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
 
-    // let x = io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Failed to readline");
-    // println!("x, ```{}```", x );
+        println!("You guessed: {}", guess);
 
-    match guess.cmp( &secret_number ) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // let x = io::stdin()
+        //     .read_line(&mut guess)
+        //     .expect("Failed to readline");
+        // println!("x, ```{}```", x );
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
 }
