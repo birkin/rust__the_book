9293fd3678d41d91ce6b0444a0ef077d8773f22d
birkin <birkin.diana@gmail.com>
2020-04-06 18:34:57 -0400

    quits with message if expected envar not found.

--- birkin_play/src/main.rs
+++ birkin_play/src/main.rs
@@ -1,34 +1,37 @@
-#[macro_use]
-// extern crate simple_error;
-
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use std::error::Error;
 
 use std::env;
-use std::ffi::OsString;
-use std::option::Option;
+use std::ffi;  // `Foreign Function Interface`
+use std::option;
+
 
 fn main() {
+
     let start = Instant::now();
 
-    // let some_var = env::var("SOME_ENVAR").is_err();
-    // let some_var: std::option::Option<std::ffi::OsString> = env::var_os("SOME_ENVAR");
-    // let some_var: option::Option<std::ffi::OsString> = env::var_os("SOME_ENVAR");
-    let some_var: Option<OsString> = env::var_os("SOME_ENVAR");
+    // get envar
+    let some_var: option::Option<ffi::OsString> = env::var_os("SOME_ENVAR");
     println!("some_var, `{:?}`", some_var);
-
     if some_var == None {
-        println!("`None` found");
-        // bail!("envars not set");
+        println!("some_var not found; quitting");
+        quit( start );
     } else {
         println!("Something found");
     }
 
+    // do work
     expensive_function();
 
+    // print duration
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is, `{:?}`", duration);
+}
+
+fn quit(start: Instant) {
+    let duration = start.elapsed();
+    println!("Time elapsed, `{:?}`", duration);
+    std::process::exit(0);
 }
 
 fn expensive_function() {
