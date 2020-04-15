11540bd8e42f87a2d0f36393205730db5a6731cf
birkin <birkin.diana@gmail.com>
2020-04-15 06:56:30 -0400

    adds struct experimentation.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,15 +1,46 @@
 fn main() {
 
-    let width1 = 30;
-    let height1 = 50;
+    // /* works, but no inherent linkage between params */
+    // let width1 = 30;
+    // let height1 = 50;
+    // println!(
+    //     "The area of the rectangle is {:?} square pixels",
+    //     area( width1, height1 )
+    //     );
 
+
+
+    // /* works, but loses sense of meaning of params in function */
+    // let rect1 = (30, 50);
+    // println!(
+    //     "The area of the rectangle is {:?} square pixels.",
+    //     area(rect1) );
+
+
+
+    let rect1 = Rectangle { width: 30, height: 50 };
     println!(
-        "The area of the rectangle is {:?} square pixels",
-        area( width1, height1 )
-        );
+        "The area of the rectangle is {:?} square pixels.",
+        area(&rect1) );
+
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area( rectangle: &Rectangle ) -> u32 {
+    rectangle.width * rectangle.height
 }
 
 
-fn area( width: u32, height: u32 ) -> u32 {
-    width * height
-}
+// fn area( dimensions: (u32, u32) ) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+
+
+// fn area( width: u32, height: u32 ) -> u32 {
+//     width * height
+// }
