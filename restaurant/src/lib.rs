2a67588676e30948671498c976ce71afc7eb99aa
birkin <birkin.diana@gmail.com>
2020-04-25 09:25:50 -0400

    explores module paths.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -56,6 +56,8 @@ mod back_of_house {
 }
 
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
@@ -73,8 +75,8 @@ pub fn eat_at_restaurant() {
     // =============
 
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    crate::hosting::add_to_waitlist();
 
     // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
