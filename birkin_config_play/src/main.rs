c3ef6dc12d2c3be2a84b24e2ec444015f03f6990
birkin <birkin.diana@gmail.com>
2020-04-26 08:59:45 -0400

    explores config setup.

--- birkin_config_play/src/main.rs
+++ birkin_config_play/src/main.rs
@@ -12,7 +12,9 @@ impl Config {
         match setting_a {
             Ok(_) => {},
             Err(_err) => {
-                println!("setting_a not found; quitting"); std::process::exit(-1); },
+                println!("setting_a not found; quitting");
+                std::process::exit(-1);
+            },
         };
         Config { setting_a }
     }
@@ -20,7 +22,17 @@ impl Config {
 
 fn main() {
     let config = Config::new();
+
+    assert_eq!( config.setting_a, Ok("bar".to_string()) );
+    // assert_eq!( config.setting_a.unwrap(), "bar".to_string() );  // problem: this _does_ get a `String`, but since the value is moved here, it can't subsequently be used  :(
+
     println!("config, ``{:?}``", config);
     println!("config.setting_a, ``{:?}``", config.setting_a );
-    // assert_eq!( config.setting_a, "bar".to_string() );
+    /*
+    Yields...
+    config, ``Config { setting_a: Ok("bar") }``
+    config.setting_a, ``Ok("bar")``
+    */
+
 }
+
