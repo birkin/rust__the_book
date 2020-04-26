24dbf02d268e3d79bfcae37ef37b8150be284083
birkin <birkin.diana@gmail.com>
2020-04-26 09:02:21 -0400

    explores config setup.

--- birkin_config_play/src/main.rs
+++ birkin_config_play/src/main.rs
@@ -8,7 +8,7 @@ struct Config {
 
 impl Config {
     fn new() -> Config {
-        let setting_a: Result<String, VarError> = env::var("ENV_FOO");
+        let setting_a: Result<String, VarError> = env::var("ENV_FOO");  // assume `$ export ENV_FOO="bar"`
         match setting_a {
             Ok(_) => {},
             Err(_err) => {
@@ -24,7 +24,7 @@ fn main() {
     let config = Config::new();
 
     assert_eq!( config.setting_a, Ok("bar".to_string()) );
-    // assert_eq!( config.setting_a.unwrap(), "bar".to_string() );  // problem: this _does_ get a `String`, but since the value is moved here, it can't subsequently be used  :(
+    // assert_eq!( config.setting_a.unwrap(), "bar".to_string() );  // problem: this _does_ get a `String`, but since the value is moved here, it can't subsequently be used.
 
     println!("config, ``{:?}``", config);
     println!("config.setting_a, ``{:?}``", config.setting_a );
