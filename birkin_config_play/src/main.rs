704541e2982051c8827d127bab152b8ab441a1fb
birkin <birkin.diana@gmail.com>
2020-04-30 06:53:57 -0400

    deletes and renames dirs.

--- birkin_config_play/src/main.rs
+++ birkin_config_play/src/main.rs
@@ -3,36 +3,36 @@ use std::env::VarError;
 
 #[derive(Debug)]
 struct Config {
-    setting_a: Result<String, VarError>,
+    setting_a: String,
 }
 
 impl Config {
     fn new() -> Config {
-        let setting_a: Result<String, VarError> = env::var("ENV_FOO");  // assume `$ export ENV_FOO="bar"`
-        match setting_a {
+        let setting_a_try: Result<String, VarError> = env::var("ENV_FOO");
+        match setting_a_try {
             Ok(_) => {},
             Err(_err) => {
                 println!("setting_a not found; quitting");
                 std::process::exit(-1);
-            },
+            }
         };
+        let setting_a = setting_a_try.unwrap();
         Config { setting_a }
     }
 }
 
 fn main() {
-    let config = Config::new();
-
-    assert_eq!( config.setting_a, Ok("bar".to_string()) );
-    // assert_eq!( config.setting_a.unwrap(), "bar".to_string() );  // problem: this _does_ get a `String`, but since the value is moved here, it can't subsequently be used.
-
-    println!("config, ``{:?}``", config);
-    println!("config.setting_a, ``{:?}``", config.setting_a );
     /*
-    Yields...
-    config, ``Config { setting_a: Ok("bar") }``
-    config.setting_a, ``Ok("bar")``
+    If envar is not set, yields output: "setting_a not found; quitting".
+    If envar is set, like: `$ export ENV_FOO="bar"`, yields output:
+        config, ``Config { setting_a: Ok("bar") }``
+        config.setting_a, ``Ok("bar")``
     */
 
-}
+    let config = Config::new();
 
+    assert_eq!( config.setting_a, "bar".to_string() );
+
+    println!("config, ``{:?}``", config);
+    println!("config.setting_a, ``{:?}``", config.setting_a);
+}
