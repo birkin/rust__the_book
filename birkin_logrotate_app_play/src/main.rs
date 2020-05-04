9a4ccb1678d9d7f53ff4cfae42cb0ced4be456b0
birkin <birkin.diana@gmail.com>
2020-05-04 07:33:55 -0400

    loads json path file.

--- birkin_logrotate_app_play/src/main.rs
+++ birkin_logrotate_app_play/src/main.rs
@@ -4,22 +4,19 @@ extern crate log;
 extern crate env_logger;
 
 use serde::Deserialize;
+use serde_json::{json, Value};
 
 use std::env;
-// use std::env::VarError;
+use std::fs;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-
-// use std::option::Option;
-
-// use std::ffi;  // `Foreign Function Interface`
-// use std::option;
 
 
 /*
 
 NEXT:
-- read json file.
+- read json file. HERE
+    - I'm reading the json file, next... review the other may-3 evening emails I sent myself showing json-load options.
 - for each entry:
     - make a reverse-time-sorted list of the existing log-files
     - delete the oldest if there are more than MAX
@@ -61,130 +58,30 @@ fn main() {
     // println!("config, ``{:?}``", config);
 
     /* logging */
-    env_logger::init();  // assumes ```export RUST_LOG="info"```
-    // debug!("logger debug test");
-    // info!("logger info test");
-    // error!("logger error test");  // only this will print if RUST_LOG is not set
-
+    env_logger::init();  // assumes ```export RUST_LOG="info"```; only error! will work if no RUST_LOG-level is set
     debug!( "{}", format!("config, ``{:#?}``", config) );  // debug! needs a string literal
 
     /* work */
-    // do work
+    // let paths = load_json();
+    // let path = concat!(env!("CARGO_MANIFEST_DIR"), "/config.json");
+    let s = fs::read_to_string( &config.logger_json_file_path ).unwrap();
+    let cfg: Value = serde_json::from_str(&s).unwrap();
+    println!("cfg, ``{:?}``", cfg);
+    println!("first-path, ``{:?}``", cfg[0]["path"]);
+
+
     expensive_function();
 
-
-    // print duration
+    /* output */
     let duration: Duration = start_time.elapsed();
     info!( "{}", format!("elapsed-time, ``{:?}``", duration) );
-
-    // println!("Time elapsed in expensive_function() is, `{:?}`", duration);
 
 }
 
 
-
-// use std::env;
-
-// let key = "KEY";
-// env::set_var(key, "VALUE");
-
-
-
-// fn load_settings( start_time: Instant ) -> String  {
-//     dotenv().ok();
-//     match envy::prefixed("LOG_ROTATOR__").from_env::<Config>() {
-//         Ok(config) => {
-//             println!("config.log_level, ``{:?}``", config.log_level);
-//             println!("{:#?}", config);
-//             // config
-//             "foo".to_string()
-//         },
-//         Err(the_error) => {
-//             // println!("error parsing config from env: {}", err);
-//             // err.to_string()
-//             // let zz: () = err;
-//             let message = "error, ```".to_string() + &the_error.to_string() + "```; quitting";
-//             // println!( message );
-//             println!("{:?}", message);
-//             // "bar".to_string()
-//             quit( start_time );
-//             message
-//         },
-//     }
-// }
-
-// fn load_settings() {
-
-//     dotenv().ok();
-//     for (key, value) in env::vars() {
-//         println!("``{}``: ``{}``", key, value);
-//     }
-
-
-//     // // get envar
-//     // let some_var = load_setting( start_time );
-//     // println!("some_var, `{:?}`", some_var);
-//     // if some_var == None {
-//     //     quit( start_time );
-
-
-
-//     // /* settings -- works
-//     //     ...but env::var_os() yields an OsString type that I'm having trouble turning into a String for the setting
-//     // */
-
-//     // // get envar
-//     // let some_var: option::Option<ffi::OsString> = env::var_os("RUST_PLAY__SOME_VAR");  // see <https://doc.rust-lang.org/std/ffi/index.html> -- I should handle Result( value, error) here.
-
-//     // println!("some_var, `{:?}`", some_var);
-
-//     // if some_var == None {
-//     //     println!("some_var not initialized; quitting");
-//     //     quit( start );
-//     // } else {
-//     //     println!("Something found");
-//     // }
-
-// }
-
-
-
-
-// fn load_setting( start_time: Instant ) -> Option<String> {
-
-//     // println!("start_time, `{:?}`", start_time);
-
-//     let some_var_try: Result<String, VarError> = env::var("RUST_PLAY__SOME_VAR");
-
-//     println!("some_var_try initially, `{:?}`", some_var_try);
-//     // let zz: () = some_var_try;  // will not compile; shows type of some_var_try. Ok, "found enum `std::result::Result`"
-
-//     match some_var_try {
-//         Ok(the_string) => the_string,
-//         // Err(the_error) => {
-//         //     panic!("Problem accessing the envar: {:?}", the_error)
-//         // },  // works, but I want to handle this
-//         // Err(the_error) => "problem".to_string(),  // works, but why can't I can't return None?
-//         Err(the_error) => {
-//             let message = "error, ```".to_string() + &the_error.to_string() + "```";  // hmm... figured out string-substitution, like in println()
-//             println!("message, {:?}", message);
-//             quit( start_time );
-//             message
-
-//         },
-//     };
-
-//     // if some_var_try == None {
-//     //     println!("some_var not initialized; quitting");
-//     //     quit( start_time );
-//     // } else {
-//     //     println!("Something found");
-//     // }
-
-//     // return Some( "foo".to_string() )
-//     return None
-// }
-
+fn expensive_function() {
+    sleep(Duration::new(0, 1)); // (seconds, nanoseconds)
+}
 
 
 // fn quit(start_time: Instant) {
@@ -192,9 +89,3 @@ fn main() {
 //     println!(" in quit(); duration, `{:?}`", duration);
 //     std::process::exit(0);
 // }
-
-
-
-fn expensive_function() {
-    sleep(Duration::new(0, 1)); // (seconds, nanoseconds)
-}
