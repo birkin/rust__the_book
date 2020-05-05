7846506851737a9d11fd6d9954f759acc6af14e2
birkin <birkin.diana@gmail.com>
2020-05-05 07:59:42 -0400

    explores hashmaps.

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -7,23 +7,62 @@ fn main() {
 
     // misc02();
 
-    misc03();
+    // misc03();
+
+    // misc04();
+
+    //  --- doesn't work ---
+    //      not a book entry; but consider exploring getting it working
+    let result: HashMap<String, i32> = misc05();
+    println!("&result ``{:?}``", &result);
+
+    for (key, value) in &result {
+        println!("{}: {}", key, value );
+    }
 
 }
 
 
-fn misc03() {
-    let field_name = String::from("Some key");
-    let field_value = 42;
+fn misc05() -> HashMap<String, i32> {
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
+    let mut scores = HashMap::new();
 
-    println!("map, ``{:?}``", map);
+    scores.insert( String::from("Blue"), 10 );
+    scores.insert( String::from("Yellow"), 50 );
 
-    // println!("field_name, ``{:?}``", field_name); // nope, because the String type ownes the value
-    println!("field_value, ``{:?}``", field_value);  // yes, because the Integer type implements the Copy trait
+    scores
+
 }
+
+
+// fn misc04() {
+
+//     let mut scores = HashMap::new();
+
+//     scores.insert( String::from("Blue"), 10 );
+//     scores.insert( String::from("Yellow"), 50 );
+
+//     let team_name = String::from("Blue");
+
+//     let score = scores.get( &team_name );
+//     println!("score, ``{:?}``", score);  // Some(10)
+
+// }
+
+
+
+// fn misc03() {
+//     let field_name = String::from("Some key");
+//     let field_value = 42;
+
+//     let mut map = HashMap::new();
+//     map.insert(field_name, field_value);
+
+//     println!("map, ``{:?}``", map);
+
+//     // println!("field_name, ``{:?}``", field_name); // nope, because the String type ownes the value
+//     println!("field_value, ``{:?}``", field_value);  // yes, because the Integer type implements the Copy trait
+// }
 
 
 
