5083071483024533303d1052ff50b3bfa1d11edf
birkin <birkin.diana@gmail.com>
2020-05-18 06:43:01 -0400

    explores generics in struct and struct-method.

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -10,24 +10,50 @@ fn main() {
     // misc03();
 
     // -- generics in structs allowing for different types
-    misc04();
+    // misc04();
+
+    // -- example of generics in structs, _and_ in a struct-method
+    misc05();
 }
 
-fn misc04() {
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+fn misc05() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    println!("both_integer-point, ``{:?}``", both_integer);
-    println!("both_float-point, ``{:?}``", both_float);
-    println!("integer_and_float-point, ``{:?}``", integer_and_float);
+    let p3 = p1.mixup( p2 );
+    println!("p3.x, ``{:?}``; p3.y, ``{:?}``", p3.x, p3.y );
 }
 
-#[derive(Debug)]
 struct Point<T, U> {
     x: T,
     y: U,
 }
+
+impl<T, U> Point<T, U> {
+    fn mixup<V,W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y
+        }
+    }
+}
+
+
+// fn misc04() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+
+//     println!("both_integer-point, ``{:?}``", both_integer);
+//     println!("both_float-point, ``{:?}``", both_float);
+//     println!("integer_and_float-point, ``{:?}``", integer_and_float);
+// }
+
+// #[derive(Debug)]
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
 
 
 
