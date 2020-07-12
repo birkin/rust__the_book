3987acf5dde8028b8f4ce353bb7e8f0d0637374b
birkin <birkin.diana@gmail.com>
2020-07-12 07:57:58 -0400

    explores creating an Iterator.

--- ch14_iterators/src/lib.rs
+++ ch14_iterators/src/lib.rs
@@ -1,4 +1,58 @@
-/* ----------------------------------
+/* ---------------------------------------------
+   START `Creating Our Own Iterators...` example */
+
+
+ #[derive(PartialEq, Debug)]
+struct Counter {
+    count: u32,
+}
+
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next( &mut self ) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some( self.count )
+        } else {
+            None
+        }
+    }
+}
+
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!( counter.next(), Some(1) );
+    assert_eq!( counter.next(), Some(2) );
+    assert_eq!( counter.next(), Some(3) );
+    assert_eq!( counter.next(), Some(4) );
+    assert_eq!( counter.next(), Some(5) );
+    assert_eq!( counter.next(), None );
+
+}
+
+// fn main() {
+//     let cntr = Counter::new();
+//     println!("cntr, ``{:?}``", cntr );
+// }
+/* END `Creating Our Own Iterators...` example
+   --------------------------------------------- */
+
+
+
+
+/* ---------------------------------------------
    START shoe filter iterator example */
 
 
@@ -8,9 +62,11 @@
     style: String,
  }
 
+
  fn shoes_in_my_size( shoes: Vec<Shoe>, shoe_size: u32 ) -> Vec<Shoe> {
     shoes.into_iter().filter( |s| s.size == shoe_size ).collect()
  }
+
 
 #[cfg(test)]
 mod tests {
@@ -56,7 +112,7 @@ mod tests {
 // fn main() {}  // don't know why book includeds this; test runs fine without it.
 
 /* END shoe filter iterator example
-   ---------------------------------- */
+   --------------------------------------------- */
 
 
 #[test]
