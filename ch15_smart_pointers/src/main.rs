a5a7151d14545043aeaed33bd188fe79e76644e9
birkin <birkin.diana@gmail.com>
2020-08-31 06:22:57 -0400

    starts strong/weak count visualization.

--- ch15_smart_pointers/src/main.rs
+++ ch15_smart_pointers/src/main.rs
@@ -36,13 +36,16 @@ fn main() {
     // misc11();
 
     // -- using weak references
-    misc12();
+    // misc12();
+
+    // -- moving branch into a different scope and visualizing strong_count and weak_count
+    misc13();
 
 }
 
 
 
-// -- for misc12()
+// -- for misc13()
 
 use std::cell::RefCell;
 use std::rc::{ Rc, Weak };
@@ -54,27 +57,79 @@ struct Node {
     children: RefCell<Vec<Rc<Node>>>,
 }
 
-fn misc12() {
+fn misc13() {
     let leaf = Rc::new( Node {
-        value: 3,
+        value = 3,
         parent: RefCell::new( Weak::new() ),
-        children: RefCell::new( vec![] )
-    } );
+        children: RefCell::new( vec![] ),
+    });
 
-    println!( "leaf parent = ``{:?}``", leaf.parent.borrow().upgrade() );
+    println!(
+        "leaf strong = ``{:?}``, weak = ``{:?}``",
+        Rc::strong_count( &leaf ),
+        Rc::weak_count( &leaf ),
+    );
 
-    let branch = Rc::new( Node {
-        value: 5,
-        parent: RefCell::new( Weak::new() ),
-        children: RefCell::new( vec![Rc::clone(&leaf)] ),
-    } );
+    {
+        let branch = Rc::new( Node {
+            value: 5,
+            parent: RefCell::new( Weak::new() ),
+            children: RefCell::new( vec![Rc::downgrade(&branch)] ),
+        });
 
-    *leaf.parent.borrow_mut() = Rc::downgrade( &branch );
+        *leaf.parent.borrow_mut() = Rc::downgrade( &branch );
 
-    println!( "leaf parent = ``{:?}``", leaf.parent.borrow().upgrade() );
-}
+        println!(
+            "branch strong = ``{:?}``, weak = ``{:?}``",
+            Rc::strong_count( &branch ),
+            Rc::weak_count( &branch ),
+        );
+
+        println!(
+            "leaf-INNER strong HEREZZ{:?}", );
+
+    }  // end `{`
+
+}  // end fn misc13()
+
+// -- end misc13()
+
+
+
+// -- for misc12()
+
+// use std::cell::RefCell;
+// use std::rc::{ Rc, Weak };
+
+// #[derive(Debug)]
+// struct Node {
+//     value: i32,
+//     parent: RefCell<Weak<Node>>,
+//     children: RefCell<Vec<Rc<Node>>>,
+// }
+
+// fn misc12() {
+//     let leaf = Rc::new( Node {
+//         value: 3,
+//         parent: RefCell::new( Weak::new() ),
+//         children: RefCell::new( vec![] )
+//     } );
+
+//     println!( "leaf parent = ``{:?}``", leaf.parent.borrow().upgrade() );
+
+//     let branch = Rc::new( Node {
+//         value: 5,
+//         parent: RefCell::new( Weak::new() ),
+//         children: RefCell::new( vec![Rc::clone(&leaf)] ),
+//     } );
+
+//     *leaf.parent.borrow_mut() = Rc::downgrade( &branch );
+
+//     println!( "leaf parent = ``{:?}``", leaf.parent.borrow().upgrade() );
+// }
 
 // -- end misc12()
+
 
 
 // -- for misc11()
